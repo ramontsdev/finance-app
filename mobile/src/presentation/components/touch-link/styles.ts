bce3f3d02ac17aa15c980614fac0617133e477d2
Ramon Nunes <ramontsdev@gmail.com>
2023-12-03 20:10:07 -0300

    app: color touch-link

--- mobile/src/presentation/components/touch-link/styles.ts
+++ mobile/src/presentation/components/touch-link/styles.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components/native";
 import { Text } from "../text";
 
-export const TouchWrap = styled.TouchableOpacity``
+export const TouchWrap = styled.TouchableOpacity``;
 
 export const Label = styled(Text)`
-  color: ${({ theme }) => theme.colors.primary.default};
+  color: ${({ theme }) => theme.colors.primary[400]};
   font-weight: 500;
-`
+`;
